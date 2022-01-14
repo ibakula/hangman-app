384c7881ae5de22e71feae7f8543391a498613d7
ibakula <ivan.bakula@outlook.com>
2022-01-14 09:00:51 +0000

    App component: state management

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,10 +1,26 @@
-import { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      playerName = "";
+    };
+    this.nameElementRef = React.createRef();
   }
   
+  handleNameChange = (e) => {
+    if (!(this.nameElementRef.current instanceof HTMLInputElement)) {
+      this.setState({ playerName: new Error("Unkown error occured. Try again.") });
+      return;
+    }
+    if (this.nameElementRef.current.value.length < 1) {
+      this.setState({ playerName: new Error("Please choose a valid name.") });
+      return;
+    }
+    this.setState({ playerName: this.nameElementRef.current.value });
+  }
+
   render() {
     return (
       <div />
