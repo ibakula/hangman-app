a7b08cbceb8bc195d62d6d4280e84d035e195c26
ibakula <ivan.bakula@outlook.com>
2022-01-14 07:00:46 +0000

    Initial work

---
+++ src/components/containers/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default App;
