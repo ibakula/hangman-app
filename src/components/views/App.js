8318e854cce0c1825aa7393e3c6de2a8170fb962
ibakula <ivan.bakula@outlook.com>
2022-01-14 13:03:24 +0000

    Fix logic

--- src/components/views/App.js
+++ src/components/views/App.js
@@ -2,41 +2,36 @@ import { Suspense, forwardRef, lazy } from 'react';
 import style from './App.css';
 
 const AppView = forwardRef(function (props, ref) {
-  if (props.playerName instanceof Error) {
+  if (props.playerName.length > 1) {
+    const GameContainer = lazy(() => import('../containers/Game'));
     return (
-      <>
+      <Suspense fallback={<div>Game starting...</div>}>
+        <GameContainer 
+          playerName={${props.playerName}} 
+        />
+      </Suspense>
+    );
+  }
+
+  return (
+    <>
+      <form> 
+        <label for="pname">Player name:</label>
+        <br /><br />
+        <input type="text" id="pname" placeholder="Player" />
+        <br /><br />
+        <input 
+           type="button" 
+           onClick={props.handleNameChange} 
+           value="Submit"
+        />
+        <br /><br />
+      </form>
+      {props.playerName instanceof Error && (<>
         <h3 className={`${style.errorMsg}`}>Couldn't start the game.</h3>
         <p>{props.playerName.message}</p>
-      </>
-    );
-  }
-  
-  if (props.playerName.length > 1) {
-    return (
-      <>
-        <form> 
-          <label for="pname">Player name:</label>
-          <br /><br />
-          <input type="text" id="pname" placeholder="Player" />
-          <br /><br />
-          <input 
-             type="button" 
-             onClick={props.handleNameChange} 
-             value="Submit"
-          />
-        </form>
-      </>
-    );
-  }
-  
-  const GameContainer = lazy(() => import('../containers/Game'));
-  // Note: Improve styling for loading message (maybe?)
-  return (
-    <Suspense fallback={<div>Game starting...</div>}>
-      <GameContainer 
-        playerName={${props.playerName}} 
-      />
-    </Suspense>
+      </>)}
+    </>
   );
 });
 
