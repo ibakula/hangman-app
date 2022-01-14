267ac8550573b1e8de632930ac35fca06ede7cee
ibakula <ivan.bakula@outlook.com>
2022-01-14 12:40:47 +0000

    Completed player registration

--- src/components/views/App.js
+++ src/components/views/App.js
@@ -12,15 +12,24 @@ const AppView = forwardRef(function (props, ref) {
   }
   
   if (props.playerName.length < 1) {
-    // Note: Form for player data
     return (
       <>
+        <form> 
+          <label for="pname">Player name:</label><br /><br />
+          <input type="text" id="pname" placeholder="Player" />
+          <br /><br />
+          <input 
+             type="button" 
+             onClick={props.handleNameChange} 
+             value="Submit"
+          />
+        </form>
       </>
     );
   }
   
   const GameContainer = lazy(() => import('../containers/Game'));
-  // Note: Improve styling for loading message
+  // Note: Improve styling for loading message (maybe?)
   return (
     <Suspense fallback={<div>Game starting...</div>}>
       <GameContainer 
