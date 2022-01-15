d2647a7e27cc6ef6e4d3a1d311d54db1d812ca0d
ibakula <ivan.bakula@outlook.com>
2022-01-15 22:52:45 +0000

    Some work on Game View component

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -4,7 +4,16 @@ import HangmanView from './Hangman';
 function GameView(props) {
   return (
     <div>
-      
+      <HangmanView 
+        mistakes={props.mistakes}
+      />
+      <WordsView 
+        content={props.content} 
+        matchedPos={props.matchedPos}
+      />
+      <form onSubmit={props.handleRestart}>
+        <input type="submit" value="Restart game" />
+      </form>
     </div>
   );
 }
