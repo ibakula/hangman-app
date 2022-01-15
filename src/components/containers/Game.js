85fba05edd1dbc6ebacb8f243295218652c56693
ibakula <ivan.bakula@outlook.com>
2022-01-15 00:58:21 +0000

    More work on game component, state management and key event handlers

--- src/components/containers/Game.js
+++ src/components/containers/Game.js
@@ -5,17 +5,39 @@ class GameContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mistakes: 0
+      mistakes: 0,
+      matchedPos: []
     };
   }
 
   handleSelectedLetter = (e) => {
-    
+    if (!(e instanceof KeyboardEvent)) {
+      return;
+    }
+
+    const lowKey = e.key.toLowerCase();
+    const matchesPos = [];
+    // Note: this test content will be replaced with an actual prop from redux
+    const testContent = "veni; vidi; vici...";
+    for (let i = 0; i < testContent.length; ++i) {
+      if (testContent[i] === lowKey) {
+        matchesPos.push(i);
+      }
+    }
+
+    if (matchesPos.length == 0) {
+      this.setState({ mistakes: (this.state.mistakes+1) });
+      return;
+    }
+
+    this.setState({ matchedPos: this.state.matchedPos.concat(matchesPos) });
   }
 
   render() {
    return (
      <GameView 
+       matchedPos={this.state.matchedPos}
+       mistakes={this.state.mistakes}
        handleSelectedLetter={this.handleSelectedLetter} 
        content={"Veni; vidi; vici...".toLowerCase()}
      />
