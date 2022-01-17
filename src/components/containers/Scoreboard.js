dbf83f61ede2b0914eb7f46a8c07ffb1a2c4e6e5
ibakula <ivan.bakula@outlook.com>
2022-01-17 00:17:02 +0000

    Some fixes to the scoreboard function

--- src/components/containers/Scoreboard.js
+++ src/components/containers/Scoreboard.js
@@ -4,7 +4,7 @@ import {
   calculateScoreByErrorCount 
 } from '../utils/calc-utils';
 
-function orderHighscores(data, orderByDesc = false) {
+function orderHighscores(data, orderByAsc = true) {
   let orderedHighscore = [];
 
   if (Array.isArray(data)) {
@@ -16,24 +16,23 @@ function orderHighscores(data, orderByDesc = false) {
 
       if (orderedHighscore.length == 0) {
         orderedHighscore.push(calculatedScore);
-        continue;
+        return;
       }
 
       for (let i = 0; i < orderedHighscore.length; ++i) {
-        if (orderByDesc && 
+        if (orderByAsc && 
           orderedHighscore[i].score > calculatedScore.score) {
           orderedHighscore.splice(i, 0, calculatedScore);
           break;
         }
-        else if (!orderedByDesc && 
+        else if (!orderByAsc && 
           orderedHighscore[i].score < calculatedScore.score) {
-          orderedHighscore.splice(i, 0, calculatedScore);
+          orderedHighscore.splice((i+1), 0, calculatedScore);
           break;
         }
 
         if (i == (orderedHighscore.length-1)) {
           orderedHighscore.push(calculatedScore);
-          break;
         }
       }
     });
