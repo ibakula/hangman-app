3398868b5f04694056515e469919be746f6e76d2
ibakula <ivan.bakula@outlook.com>
2022-01-19 11:46:44 +0000

    Writing tests

---
+++ src/calculation.test.js
@@ -0,0 +1,18 @@
+import { computeScore } from './components/utils/calc-utils.js'
+import { orderHighscores } from './components/utils/sorting-utils.js';
+
+it("Solution with fewer errors should always be higher", function() {
+  
+});
+
+it("Solution with same number of errors, but larger number of unique letters should be scored higher", function() {
+  
+});
+
+it("Solution with same number of errors, unique letters but longer should be scored higher", function() {
+  
+});
+
+it("Solution with same number of errors, unique letters and quote length but lower duration should be scored higher", function() {
+  
+});
